9f9160d7fff1ed67a5e1f91a4ca24f6442efa723
zwobotten <jluetgev@gmail.com>
2019-08-08 14:48:32 +0200

    first attempt csv to json, still need to fix output format

--- csv_reader.py
+++ csv_reader.py
@@ -1,37 +1,11 @@
-import csv
 import os
-import json
+from csv_convert import CSVToJson
 
 
-def main():
-    name = 'example-export-reduced'
-    filename = get_data_file(name)
-    load_file(filename)
-    # query_data(data)
+def convert():
+    csv_convert = CSVToJson()
+    csv_file = open('./testdata/example.csv')
+
+    json_output = csv_convert.start(csv_file, ['date', 'time', 'licencePlate', 'additionalInfo', 'Amount', 'grossPrice'])
 
 
-def get_data_file(name):
-    data = []
-    filename = os.path.abspath(os.path.join('.', 'testdata', name + '.csv'))
-
-    return filename
-
-
-def load_file(filename):
-    #TODO fix output
-    file = open(filename, 'r', newline='', encoding='utf-8')
-    reader = csv.DictReader(file, fieldnames= ('date','time','licencePlate','additionalInfo','amount','grossPrice'))
-    out = json.dumps([row for row in reader])
-    print('Json parsed!')
-    name = 'examplejson'
-    file = open(os.path.abspath(os.path.join('.', 'testdata', name + '.json')),'w')
-    file.write(out)
-    print('Json saved!')
-
-
-def query_data():
-    pass
-
-
-if __name__ == '__main__':
-    main()
