617989dab7096ffd2ac85a1e261890d80e6c2e13
zwobotten <jluetgev@gmail.com>
2019-08-08 14:56:19 +0200

    first attempt csv to json, still need to fix output format

---
+++ program.py
@@ -0,0 +1,12 @@
+def main():
+    pass
+    #csv_reader.convert()
+    #merge date/time
+    #take min/max timestamps for endTime (/reservations)
+    #connect.db_query()
+    #wunder.get_plates() (loop through db_query results to get plates for carids)
+    #match plates from get_plates() with plates from convert() & check against timestamps
+    #wunder.post_vouchers and pass strings
+
+if __name__ == '__main__':
+    main()
