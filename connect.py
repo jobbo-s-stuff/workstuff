274aa7a48749b7b6b472045fefade2b4e10f4287
iammmai <dtmai@gmx.de>
2019-08-15 23:26:06 +0200

    User string adaptation instead of concatenation

--- connect.py
+++ connect.py
@@ -2,10 +2,6 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import program
 
-min_fuel_time = ''
-max_fuel_time = ''
-licence_plates = []
-
 '''
 Creates connection to db, queries and joins required car, reservation data and dumps it into json
 '''
@@ -20,15 +16,7 @@ def db_query(min_fuel_time, max_fuel_time, licence_plates):
                                   database="database")
     cursor = connection.cursor(cursor_factory=RealDictCursor)
     print("Querying reservations, cars...")
-    # TODO: pass list as string
-    car_res = "SELECT cars.carid, cars.licenceplate, cars.vehicletypeid, " \
-              "reservations.reservationid, reservations.carid, reservations.customerid, reservations.starttime, " \
-              "reservations.endtime, reservations.startbatterylevel, reservations.endbatterylevel, " \
-              "FROM reservations INNER JOIN cars ON reservations.carid = cars.carid  " \
-              "WHERE reservations.endtime >= " + "'" + min_fuel_time + "'" + " and reservations.starttime <=" + "'" + max_fuel_time + "'" + \
-              " and startbatterylevel <= 25  and endbatterylevel >= 84 and isdev = 0 and drivendistance != 0" \
-              " and cars.licenceplate IN" + ','.join(licence_plates) + ";"
-    cursor.execute(car_res)
+    cursor.execute("SELECT cars.carid, cars.licenceplate, cars.vehicletypeid, reservations.reservationid, reservations.carid, reservations.customerid, reservations.starttime, reservations.endtime, reservations.startbatterylevel, reservations.endbatterylevel FROM reservations INNER JOIN cars ON reservations.carid = cars.carid WHERE reservations.endtime >= (%s) and reservations.starttime <= (%s)and startbatterylevel <= 25  and endbatterylevel >= 84 and isdev = 0 and drivendistance != 0 and cars.licenceplate IN ANY(%s);", min_fuel_time, max_fuel_time, licence_plates)
 
     # TODO: don't dump as json, store in list instead
 
