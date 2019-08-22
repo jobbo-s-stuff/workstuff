a85e602c98714acc4678601e1143310a9e84ad5f
zwobotten <jluetgev@gmail.com>
2019-08-22 17:04:32 +0200

    Merge branch 'Maichis_branch' of https://github.com/jobbo-s-stuff/workstuff into develop
    # Conflicts:
    #	csv_reader.py
    #	program.py

--- fuel_class.py
+++ fuel_class.py
@@ -4,6 +4,10 @@ class FuelEvent:
         self.plate_fallback = plate_fallback
         self.date_time = date_time
 
+    def __str__(self):
+        return "licence_plate:" + self.licence_plate + ", plate_fallback:" + self.plate_fallback + ", datetime:" + self.date_time.strftime(
+            "%m/%d/%Y, %H:%M:%S")
+
     @staticmethod
     def create_from_dict(lookup):
         return FuelEvent(
--- wunder.py
+++ wunder.py
@@ -1,18 +1,29 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
-'''
-Post to backend API /vouchers endpoint
-'''
+# TODO: create fake data set for testing
 
-def post_vouchers(vouchers):
+url = "https://url/rest/credit-voucher/create"
+data = '{"description": "Test|16.08.2019|771", "customerId": "89", "value": "500"}'
+headers = {'x-api-key': 'key', 'Content-Type': 'application/x-www-form-urlencoded' }
+
+
+def post_vouchers():
+    req = requests.post(url, data=data, headers=headers)
+    print(req.status_code, req.reason, req._content)
+
+
     # loop through list and convert (unless easier to convert to JSON beforehand)
     # post request to /credit-voucher/create
     # request format:
-        # "description": "Tanken|dd.MM.yyyy|reservationId"
-        # "customerId": int
-        # "value": 500
+    # "description": "Tanken|dd.MM.yyyy|reservationId"
+    # "customerId": int
+    # "value": 500
     # create fuel_log_<date>.txt and pass today
     # append successes and failures to fuel_log_<date>.txt:
-        # print("Voucher 'creditVoucherId' successfully created for customer 'customerId'.")
-        # print("Voucher not created successfully for customer 'customerId'.")
-    pass
+    # print("Voucher 'creditVoucherId' successfully created for customer 'customerId'.")
+    # print("Voucher not created successfully for customer 'customerId'.")
+    return url
+
+
+post_vouchers()
