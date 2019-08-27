d349855e9908ce5463f766d868836d4a9125d654
zwobotten <jluetgev@gmail.com>
2019-08-27 15:41:15 +0200

    Merge branch 'Maichis_branch' of https://github.com/jobbo-s-stuff/workstuff into develop
    # Conflicts:
    #	csv_reader.py
    #	program.py

--- program.py
+++ program.py
@@ -2,72 +2,53 @@ import os
 import csv_reader
 import connect
 import wunder
+from psycopg2.extras import RealDictRow
 
 
 def main():
-    in_filename = get_csv_in()
-    out_filename = get_csv_out()
-    events = csv_reader.clean_read_fuel_events(in_filename)
-    #min_fuel_time = get_min_time(events)
-    #max_fuel_time = get_max_time(events)
-    #licence_plates = get_plates(events)
-    #connect.db_query()  # pass timestamps, endtime between earliest and latest fuel timestamp, use lambda,
-                        # pass licence plates collection
-
-    # select reservatons that match licence plate and fueltimestamp falls between start and end time
-    # convert to API consumable
-    #wunder.post_vouchers()
+    in_filename = get_csv_in()  # identify input csv
+    events = csv_reader.clean_read_fuel_events(in_filename)  # cleans csv and returns rows as events
+    print()
+    print('CSV read. Continue? (Y/N)')
+    step = input()
+    if step == 'Y':
+        min_fuel_time = csv_reader.get_min_time(events)  # returns earliest timestamp
+        max_fuel_time = csv_reader.get_max_time(events)  # returns latest timestamp
+    elif step == 'N':
+        exit()
+    print()
+    print('Timeframe identified. Continue? (Y/N)')
+    step = input()
+    if step == 'Y':
+        licence_plates = csv_reader.get_plates(events)  # finds and replaces plates with backup where necessary
+    elif step == 'N':
+        exit()
+    print()
+    print('Licence Plates corrected. Continue? (Y/N)')
+    step = input()
+    if step == 'Y':
+        query_result = connect.db_query(min_fuel_time, max_fuel_time, licence_plates) # passes data to db query
+        if len(query_result) > len(events): # in case of missing fuel events
+            raise ValueError('Query result does not match fuel events')
+    elif step == 'N':
+        exit()
+    print()
+    print('Query data prepared. Continue? (Y/N)')
+    if step == 'Y':
+        # wunder.post_vouchers(query_result)  # loops through query results to create vouchers
+        exit()
+    elif step == 'N':
+        exit()
 
 
 def get_csv_in():
     base_folder = os.path.dirname(__file__)
-    return os.path.join(base_folder, 'testdata', 'example_input.csv')
+    return os.path.join(base_folder, 'data', 'export-cleaned.csv')
 
 
 def get_csv_out():
     base_folder = os.path.dirname(__file__)
     return os.path.join(base_folder, 'testdata', 'example_output.csv')
-
-
-def get_min_time(events):
-    min_time = ''
-    for e in events:
-        if min_time == '':
-            min_time = e.date_time
-        elif e.date_time < min_time:
-            min_time = e.date_time
-
-    print(min_time)
-    return min_time
-
-
-def get_max_time(events):
-    # determine maximum timestamp
-    # return
-    pass
-
-
-def get_plates(events):
-    # create licence plate array
-    # return
-    pass
-
-
-def res_match():
-    # loop through connect output, compare with csv_reader output
-        # match reservation where:
-            # licencePlate identical, ignore blank spaces
-            # fuelTimeStamp >= startTime and <= endTime
-    # pass reservationid, customerid, datetime to list for voucher_convert
-    # TODO: maybe again use class (s. csv_reader todo)
-    pass
-
-
-def voucher_convert():
-    # loop through list from res_match
-    # convert to json
-    # TODO: check whether easier and more sensible to convert in post method
-    pass
 
 
 if __name__ == '__main__':
